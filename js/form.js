4014a18319f4a357b9dfd35c9a0c261b50355e7f
Jackscript126 <112936185+Jackscript126@users.noreply.github.com>
2023-03-29 20:22:28 +0300

    ДЗ номер 9

---
+++ js/form.js
@@ -0,0 +1,55 @@
+import {activateScale, resetScale} from './form-scale.js';
+import {changeEffect, resetFilter, createSlider} from './form-effects.js';
+import {addValidator, resetPristine, validatePristine} from './form-validate.js';
+
+const form = document.querySelector('.img-upload__form');
+const overlay = document.querySelector('.img-upload__overlay');
+const cancelButton = document.querySelector('#upload-cancel');
+const fileField = document.querySelector('#upload-file');
+const effectsField = document.querySelector('.effects');
+
+const onDocumentKeydown = (evt) => {
+  if (evt.key === 'Escape' && !evt.target.closest('.text__hashtags') && !evt.target.closest('.text__description')) {
+    evt.preventDefault();
+    closeModal();
+  }
+};
+
+const onCancelButtonClick = () => closeModal();
+const onEffectsFieldChange = (evt) => changeEffect(evt);
+
+const onFormSubmit = (evt) => {
+  if (!validatePristine()) {
+    evt.preventDefault();
+  }
+};
+
+const openModal = () => {
+  overlay.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+const onFileInputChange = () => openModal();
+
+function closeModal() {
+  form.reset();
+  resetScale();
+  resetFilter();
+  resetPristine();
+  overlay.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+}
+
+const addFormAction = () => {
+  fileField.addEventListener('change', onFileInputChange);
+  cancelButton.addEventListener('click', onCancelButtonClick);
+  effectsField.addEventListener('change', onEffectsFieldChange);
+  form.addEventListener('submit', onFormSubmit);
+  activateScale();
+  addValidator();
+  createSlider();
+};
+
+export {addFormAction};
