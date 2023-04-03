89ae77ed215c09c9f35600e26de4121527947495
Jackscript126 <112936185+Jackscript126@users.noreply.github.com>
2023-04-03 20:03:21 +0300

    Добавляет ДЗ № 11

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,9 +1,10 @@
-import { createPhotos } from './data.js';
-import { openBigPicture } from './big-picture.js';
+import {openBigPicture} from './big-picture.js';
+import {getData} from './api.js';
 
+const GET_URL = 'https://28.javascript.pages.academy/kekstagram/data';
+const ERROR_TIMEOUT = 5000;
 const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const container = document.querySelector('.pictures');
-const data = createPhotos();
 
 const createThumbnail = (item) => {
   const thumbnail = thumbnailTemplate.cloneNode(true);
@@ -18,6 +19,31 @@ const createThumbnail = (item) => {
   return thumbnail;
 };
 
-const renderThumbnails = () => data.forEach((item) => container.append(createThumbnail(item)));
+const renderThumbnails = (data) => {
+  data.forEach((item) => container.append(createThumbnail(item)));
+};
 
-export { renderThumbnails };
+const onGetSuccess = (data) => renderThumbnails(data);
+
+const onGetFail = () => {
+  const errorBlock = document.createElement('div');
+  errorBlock.style.position = 'fixed';
+  errorBlock.style.top = '0';
+  errorBlock.style.left = '0';
+  errorBlock.style.width = '100%';
+  errorBlock.style.height = '60px';
+  errorBlock.style.color = 'red';
+  errorBlock.style.textAlign = 'center';
+  errorBlock.style.padding = '20px';
+  errorBlock.style.backgroundColor = 'white';
+  errorBlock.textContent = 'Произошла ошибка загрузки';
+  document.body.append(errorBlock);
+
+  setTimeout(() => {
+    errorBlock.remove();
+  }, ERROR_TIMEOUT);
+};
+
+const getPicturesData = () => getData(GET_URL, onGetSuccess, onGetFail);
+
+export {getPicturesData};
