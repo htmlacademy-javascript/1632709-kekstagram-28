30a56a2b77c0550559940f2d6fc98fcb499b44b1
Jackscript126 <112936185+Jackscript126@users.noreply.github.com>
2023-04-04 14:23:46 +0300

    Добавляет отправку данных из формы

--- js/form.js
+++ js/form.js
@@ -1,15 +1,30 @@
 import {activateScale, resetScale} from './form-scale.js';
 import {changeEffect, resetFilter, createSlider} from './form-effects.js';
 import {addValidator, resetPristine, validatePristine} from './form-validate.js';
+import {renderFailMessage, renderSuccessMessage} from './send-messages.js';
+import {sendData} from './api.js';
 
+const GET_URL = 'https://28.javascript.pages.academy/kekstagram';
 const form = document.querySelector('.img-upload__form');
 const overlay = document.querySelector('.img-upload__overlay');
 const cancelButton = document.querySelector('#upload-cancel');
 const fileField = document.querySelector('#upload-file');
 const effectsField = document.querySelector('.effects');
 
+const onSendSuccess = () => {
+  renderSuccessMessage();
+  closeModal();
+};
+
+const onSendFail = () => {
+  renderFailMessage();
+};
+
 const onDocumentKeydown = (evt) => {
   if (evt.key === 'Escape' && !evt.target.closest('.text__hashtags') && !evt.target.closest('.text__description')) {
+    if (document.querySelector('.error')) {
+      return;
+    }
     evt.preventDefault();
     closeModal();
   }
@@ -19,8 +34,9 @@ const onCancelButtonClick = () => closeModal();
 const onEffectsFieldChange = (evt) => changeEffect(evt);
 
 const onFormSubmit = (evt) => {
-  if (!validatePristine()) {
-    evt.preventDefault();
+  evt.preventDefault();
+  if (validatePristine()) {
+    sendData(GET_URL, onSendSuccess, onSendFail, new FormData(evt.target));
   }
 };
 
