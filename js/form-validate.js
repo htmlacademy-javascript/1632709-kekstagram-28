057315f171a132ae2e9971fbb219e9a5df164047
Jackscript126 <112936185+Jackscript126@users.noreply.github.com>
2023-04-06 21:32:03 +0300

    Исправляет замечание по защите проекта

--- js/form-validate.js
+++ js/form-validate.js
@@ -66,5 +66,4 @@ const addValidator = () => {
 const resetPristine = () => pristine.reset();
 const validatePristine = () => pristine.validate();
 
-
 export {addValidator, resetPristine, validatePristine};
